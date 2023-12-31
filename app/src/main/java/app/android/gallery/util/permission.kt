0d2da4131b1585c2b2e28c3b9d727a2dd1fce43e
fesowowako <118536485+fesowowako@users.noreply.github.com>
2023-12-31 12:49:14 +0200

    Update permission.kt

--- app/src/main/java/app/android/gallery/util/permission.kt
+++ app/src/main/java/app/android/gallery/util/permission.kt
@@ -6,18 +6,15 @@ import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 
 fun Activity.checkPermissions(permissions: Array<String>): Boolean {
-    if (permissions.isEmpty()) return true
-    if (!hasPermission(permissions.first())) {
-        ActivityCompat.requestPermissions(
-            this,
-            permissions,
-            1
-        )
-        return false
-    }
-    return true
+   val permissionsToRequest = permissions.filter { !hasPermission(it) }.toTypedArray()
+
+   if (permissionsToRequest.isNotEmpty()) {
+       ActivityCompat.requestPermissions(this, permissionsToRequest, 1)
+       return false
+   }
+   return true
 }
 
 fun Context.hasPermission(permission: String): Boolean {
-    return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+   return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
 }
