cc9012ce918b9c928adb1460c58fbd734e3afd11
fesowowako <118536485+fesowowako@users.noreply.github.com>
2023-12-31 13:50:37 +0200

    Update observe.kt

--- app/src/main/java/app/android/gallery/util/observe.kt
+++ app/src/main/java/app/android/gallery/util/observe.kt
@@ -7,15 +7,16 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 
 fun ContentResolver.observe(uri: Uri) = callbackFlow {
-    val observer = object : ContentObserver(null) {
-        override fun onChange(selfChange: Boolean) {
-            trySend(selfChange)
-        }
-    }
-    registerContentObserver(uri, true, observer)
-    // trigger first.
-    trySend(false)
-    awaitClose {
-        unregisterContentObserver(observer)
-    }
+  val scope = CoroutineScope(Dispatchers.IO)
+  val observer = object : ContentObserver(null) {
+      override fun onChange(selfChange: Boolean) {
+          trySend(selfChange)
+      }
+  }
+  registerContentObserver(uri, true, observer)
+  // trigger first.
+  trySend(false)
+  awaitClose {
+      unregisterContentObserver(observer)
+  }
 }
